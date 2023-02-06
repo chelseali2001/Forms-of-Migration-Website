bd107addd257bad0172277511223b00ae7b13921
Chelsea Li <chelseali2001@gmail.com>
2023-02-06 13:12:22 -0800

    Adding links

--- src/App.js
+++ src/App.js
@@ -2,63 +2,78 @@ import logo from './logo.svg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookSquare, faTwitterSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
+import {
+  Routes,
+  Route,
+  Link,
+  useParams,
+  Outlet,
+  Navigate
+} from 'react-router-dom'
 
+function NotFound() {
+  return <h1>404: Page Not Found</h1>
+}
+function Home() {}
+function InstitutionsandAffiliates() {}
+function Publications() {}
+function Artworks() {}
+function AudiosandVideos() {}
+function RecentNewsandEvents() {}
+function Programs() {}
 
 function App() {
   return (
-    <div className="App">
-      <div className="home-page" >
-        <img src="https://cdn-icons-png.flaticon.com/512/91/91819.png" alt="icon" className="icon" />
+      <>
+        <div className="App">
+          <div className="home-page" >
+            <img src="https://cdn-icons-png.flaticon.com/512/91/91819.png" alt="icon" className="icon" />
         
-        <h1>Migration</h1>
-        <div className="search-container">
-          <input type="text" placeholder="Search" />
-          <button>&#128269;</button>
-        </div>
+            <h1>Migration</h1>
+            <div className="search-container">
+              <input type="text" placeholder="Search" />
+              <button>&#128269;</button>
+            </div>
 
+            <div className= "tabs">
+              <div className="rectangle-container">
+                <Link to="/institutions-&-affiliates" className='rectangle'>Institutions & Affiliates</Link>
+                <Link to="/publications" className='rectangle'>Publications</Link>
+                <Link to="/artworks" className='rectangle'>Artworks</Link>
+                <Link to="/audios-&-videos" className='rectangle'>Audios & Videos</Link>
+                <Link to="/recent-news-&-events" className='rectangle'>Recent News & Events</Link>
+                <Link to="/programs" className='rectangle'>Programs</Link>
+              </div>
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="/" element={<Navigate to="/" />} />
+                <Route path="/institutions-&-affiliates" element={<InstitutionsandAffiliates />} />
+                <Route path="/publications" element={<Publications />} />
+                <Route path="/artworks" element={<Artworks />} />
+                <Route path="/audios-&-videos" element={<AudiosandVideos />} />
+                <Route path="/recent-news-&-events" element={<RecentNewsandEvents />} />
+                <Route path="/programs" element={<Programs />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </div>
 
-      <div className= "tabs">
-        <div className="rectangle-container">
-          <div className="rectangle">
-          <h2>Institutions & Affiliates</h2>
+          <footer className="footer">
+            <div className="footer-left">
+              <p>Copyright</p>
+              <p>Contact Info</p>
+            </div>
+            <div className="footer-right">
+              <p>Follow Us</p>
+              <div className="footer-icons">
+              <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
+              <FontAwesomeIcon icon={faTwitterSquare} size="2x" />
+              <FontAwesomeIcon icon={faInstagramSquare} size="2x" />
+              </div>
+            </div>
+          </footer>
         </div>
-        <div className="rectangle">
-          <h2>Publications</h2>
-        </div>
-        <div className="rectangle">
-          <h2>Artworks</h2>
-        </div>
-        <div className="rectangle">
-          <h2>Audios & Videos</h2>
-        </div>
-        <div className="rectangle">
-          <h2>Recent News & Events</h2>
-        </div>
-        <div className="rectangle">
-          <h2>Programs</h2>
-        </div>
-      </div>
-    </div>
-      </div>
-
-
-      <footer className="footer">
-      <div className="footer-left">
-        <p>Copyright</p>
-        <p>Contact Info</p>
-      </div>
-      <div className="footer-right">
-        <p>Follow Us</p>
-        <div className="footer-icons">
-        <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
-        <FontAwesomeIcon icon={faTwitterSquare} size="2x" />
-        <FontAwesomeIcon icon={faInstagramSquare} size="2x" />
-
-        </div>
-      </div>
-    </footer>
-    </div>
-  );
+      </>
+  )
 }
-
 export default App;
