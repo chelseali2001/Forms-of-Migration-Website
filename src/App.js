b9e4abaeb61163f79a315e4d1ec275d6050363ad
haichao-joy <yanghai@oregonstate.edu>
2023-02-10 19:59:52 -0800

    add icons to all tabs on homepage

--- src/App.js
+++ src/App.js
@@ -44,11 +44,17 @@ function App() {
             <div className= "tabs">
               <div className="rectangle-container">
                 <Link to="/institutions-&-affiliates" className='rectangle'>Institutions & Affiliates</Link>
+                <img src={require('./images/institution.png')} alt="tabIcon" className='tabIcon' width="70" height="70" />
                 <Link to="/publications" className='rectangle'>Publications</Link>
+                <img src={require('./images/publication.png')} alt="tabIcon" className='tabIcon' width="70" height="70" />
                 <Link to="/artworks" className='rectangle'>Artworks</Link>
+                <img src={require('./images/artwork.png')} alt="tabIcon" className='tabIcon' width="70" height="70" />
                 <Link to="/audios-&-videos" className='rectangle'>Audios & Videos</Link>
+                <img src={require('./images/video.png')} alt="tabIcon" className='tabIcon' width="70" height="70" />
                 <Link to="/recent-news-&-events" className='rectangle'>Recent News & Events</Link>
+                <img src={require('./images/news.png')} alt="tabIcon" className='tabIcon' width="70" height="70" />
                 <Link to="/programs" className='rectangle'>Programs</Link>
+                <img src={require('./images/program.png')} alt="tabIcon" className='tabIcon' width="70" height="70" />
               </div>
               <Routes>
                 <Route index element={<Home />} />
