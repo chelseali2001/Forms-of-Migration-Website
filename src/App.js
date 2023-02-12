f9d699b21688a6addd510d5b3186969c66c084c3
Chelsea Li <chelseali2001@gmail.com>
2023-02-12 11:56:11 -0800

    Keeping everything functioning on the same page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faTwitterSquare, faInstagramSquare } from '@fortawesome/free-brands-svg-icons'
 import {
-  Routes,
-  Route,
-  Link,
-  useParams,
-  Outlet,
-  Navigate
-} from 'react-router-dom'
+    Routes,
+    Route,
+    Link,
+  } from 'react-router-dom'
 
 import HomePage from './components/HomePage'
 import InstitutionsandAffiliates from './components/InstitutionsandAffiliates'
@@ -17,25 +15,56 @@ import AudiosandVideos from './components/AudiosandVideos'
 import RecentNewsandEvents from './components/RecentNewsandEvents'
 import Programs from './components/Programs' 
 import ExcerptsFromBook from './components/ExcerptsFromBook'
-
-function NotFound() {
-  return <h1>404: Page Not Found</h1>
-}
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-              <Routes>
-                <Route index element={<HomePage />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="/institutions-&-affiliates" element={<InstitutionsandAffiliates />} />
-                <Route path="/publications" element={<Publications />} />
-                <Route path="/artworks" element={<Artworks />} />
-                <Route path="/audios-&-videos" element={<AudiosandVideos />} />
-                <Route path="/recent-news-&-events" element={<RecentNewsandEvents />} />
-                <Route path="/programs" element={<Programs />} />
-                <Route path="/excerpts-from-book" element={<ExcerptsFromBook />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
+    <>
+      <div className="App">
+        <div className="home-page" >
+          <img src="https://cdn-icons-png.flaticon.com/512/91/91819.png" alt="icon" className="icon" />  
+            <Link to="/" className='logoLink'>Migration</Link>
+      
+          <div className="search-container">
+            <input type="text" placeholder="Search" />
+            <button>&#128269;</button>
+          </div>
+
+          <div className='book-tab'>
+            <Link to="/excerpts-from-book" className='excerpts'>Excerpts From Book</Link>
+          </div>
+        </div>
+
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/institutions-&-affiliates" element={<InstitutionsandAffiliates />} />
+          <Route path="/publications" element={<Publications />} />
+          <Route path="/artworks" element={<Artworks />} />
+          <Route path="/audios-&-videos" element={<AudiosandVideos />} />
+          <Route path="/recent-news-&-events" element={<RecentNewsandEvents />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/excerpts-from-book" element={<ExcerptsFromBook />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+
+        <footer className="footer">
+          <div className="footer-left">
+            <p>Copyright</p>
+            <p>Contact Info</p>
+          </div>
+          
+          <div className="footer-right">
+            <p>Follow Us</p>
+            <div className="footer-icons">
+              <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
+              <FontAwesomeIcon icon={faTwitterSquare} size="2x" />
+              <FontAwesomeIcon icon={faInstagramSquare} size="2x" />
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
   )
 }
 export default App;
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageNotFound = () => (
+    <h1>404 Error: Page Not Found</h1>
+);
+
+export default PageNotFound;
