56be7ddded685d72df9632d67e98d5a0146e0967
SJ <SJ>
2023-02-23 17:10:10 -0800

    added static functionality for publications page

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './styling/Publications.css';
 
-const Publications = () => (
-  <div style={{backgroundColor: "white", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
-    <h1>Publications</h1>
-  </div>
-);
+const publicationsData = [
+  { id: 1, url: 'https://example.com/publication-1' },
+  { id: 2, url: 'https://example.com/publication-2' },
+  { id: 3, url: 'https://example.com/publication-3' },
+];
+
+const Publications = () => {
+  return (
+    <div className="publications">
+      <h2>Publications</h2>
+      <div className="publications-table">
+        {publicationsData.map((publication) => (
+          <div className="publications-row" key={publication.id}>
+            <div className="publications-number">{publication.id}</div>
+            <div className="publications-url">{publication.url}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Publications;
