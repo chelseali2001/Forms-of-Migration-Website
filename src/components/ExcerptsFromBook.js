46af43787174867e6817c2dda9cc689a0e515e75
haichao-joy <yanghai@oregonstate.edu>
2023-02-25 23:33:11 -0800

    add button that clicks to show the book pdf file locally for user

--- src/components/ExcerptsFromBook.js
+++ src/components/ExcerptsFromBook.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import file from "../FormOfMigration.pdf";
 
-const ExcerptsFromBook = () => (
-  <div style={{backgroundColor: "white", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
-    <h1>Excerpts From Book</h1>
-  </div>
-);
+const ExcerptsFromBook = () => {
+  const openNewTab = (url) => {
+    window.open(url, '_blank', 'noreferrer');
+  };
+  function showpdf(file){
+    window.open(file);
+  }
+  return (
+    <div style={{backgroundColor: "white", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      {/* <h1>Excerpts From Book</h1> */}
+      {/* <a href="https://drive.google.com/file/d/1uzWdwzS4gRQeSw9jD1qDCHd1Tx0BavYM/view" target="_blank" rel="noreferrer">Forms of Migration Book</a> */}
+      <button role="link" onClick={() => openNewTab('https://drive.google.com/file/d/1uzWdwzS4gRQeSw9jD1qDCHd1Tx0BavYM/view')}>Forms of Migration Book</button>
+      <button onClick={() => showpdf(file)}>book</button>
+    </div>
+  );
+}
 
 export default ExcerptsFromBook;
