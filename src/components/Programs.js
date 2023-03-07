bce633e9b32e3e36e2ff8f1cd03fe56702f70adb
Chelsea Li <chelseali2001@gmail.com>
2023-03-06 21:39:58 -0800

    Added start and end date column for Programs

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -3,9 +3,9 @@ import './styling/Programs.css';
 
 
 const programsData = [
-  { id: 1, name: 'program1', link: 'https://www.migrationpolicy.org/programs'},
-  { id: 2, name: 'program2', link: 'https://results.ed.gov/' },
-  { id: 3, name: 'program3', link: 'https://www.humanrightscareers.com/magazine/top-10-masters-programs-in-migration-studies/' },
+  { id: 1, name: 'program1', start_date: '03/06/2023', end_date: '03/06/2023', link: 'https://www.migrationpolicy.org/programs'},
+  { id: 2, name: 'program2', start_date: '03/06/2023', end_date: '03/06/2023', link: 'https://results.ed.gov/' },
+  { id: 3, name: 'program3', start_date: '03/06/2023', end_date: '03/06/2023', link: 'https://www.humanrightscareers.com/magazine/top-10-masters-programs-in-migration-studies/' },
 ];
 
 const Programs = () => {
@@ -17,6 +17,8 @@ const Programs = () => {
           <tr>
             <th>ID</th>
             <th>Name</th>
+            <th>Start Date</th>
+            <th>End Date</th>
             <th>Link</th>
           </tr>
         </thead>
@@ -25,6 +27,8 @@ const Programs = () => {
             <tr>
               <td>{program.id}</td>
               <td>{program.name}</td>
+              <td>{program.start_date}</td>
+              <td>{program.end_date}</td>
               <td>
                 <a href={program.link} target="_blank">link</a>
               </td>
