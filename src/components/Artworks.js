b7f392e9044d47d0e75ba0fa2dfcc987c7f9af7d
Chelsea Li <chelseali2001@gmail.com>
2023-03-17 15:23:05 -0700

    Changing artworks page to be able to handle the right data types from the backend

--- src/components/Artworks.js
+++ src/components/Artworks.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 import './styling/Artworks.css';
 
 const Artworks = () => {
+  const [selectedArtwork, setSelectedArtwork] = useState(null);
   const [allArtworks, setallArtworks] = useState([]);
-
   const USE_EFFECT_ONLY_ON_MOUNT = Array.from([])
 
   // useEffect(() => {
@@ -21,30 +21,43 @@ const Artworks = () => {
   // Test data - TODO: remove this and uncomment above useEffect
   useEffect(() => {
     setallArtworks([
-      "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-      "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-      "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+      {id: 1, title: "Artwork 1", author: "Jennifer Reimer Recio, Stefan Maneval", date: "2022-03-16", image: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"},
+      {id: 2, title: "Artwork 2", author: "Jennifer Reimer Recio, Stefan Maneval", date: "2022-03-16", image: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80"},
+      {id: 3, title: "Artwork 3", author: "Jennifer Reimer Recio, Stefan Maneval", date: "2022-03-16", image: "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"},
     ])
   }, USE_EFFECT_ONLY_ON_MOUNT);
+
+  const handleRowClick = (artwork) => {
+    setSelectedArtwork(artwork);
+  };
+
+  const closeModal = () => {
+    setSelectedArtwork(null);
+  };
 
   return (
   <>
     <h1>Artworks</h1>
     <div className='gallery'>
-      <img className='image' src={allArtworks[0]} alt="1"/>
-      <img className='image' src={allArtworks[1]} alt="2"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
-      <img className='image' src={allArtworks[2]} alt="3"/>
+      {allArtworks.map((anArtwork) => (
+        <img className='image' key={anArtwork.id} onClick={() => handleRowClick(anArtwork)} src={anArtwork.image} alt={anArtwork.id} />
+      ))}
     </div>
+    {selectedArtwork && (
+        <div className="modalBackdrop">
+          <div className="modal">
+            <div className="modalHeader">
+              <button onClick={closeModal} className="closeButton">X</button>
+            </div>
+            <div className="modalContent">
+              <p><strong>Title: </strong>{selectedArtwork.title}</p>
+              <img className='selected-image' src={selectedArtwork.image} alt={selectedArtwork.id} />
+              <p><strong>Author: </strong>{selectedArtwork.author}</p>
+              <p><strong>Date: </strong>{selectedArtwork.date}</p>
+            </div>
+          </div>
+        </div>
+    )}
   </>
   );
 };
